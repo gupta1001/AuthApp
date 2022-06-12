d66c762f7d258783d97ec242420231a0aa7ed269
Prankur Gupta <prankurgupta@Prankurs-MacBook-Pro.local>
2022-06-12 19:35:23 +0530

    Add level 4 security with hashing+salting and salt rounds

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+
+//encryption using bcrypt Hashing+Salting
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+//For using md5 hashing algorithms
+// const md5 = require("md5");
 
 //For mongoose encryption
 /*const encrypt = require("mongoose-encryption");*/
@@ -17,7 +22,7 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema =  new mongoose.Schema({
     email: String,
     password: String
-}) ; 
+}); 
 
 //encrypting the database passwords of user using mongoose-encryption
 /*userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});*/
@@ -38,31 +43,57 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render("secrets"); 
+            }
+        });
     });
-    newUser.save(function (err) {
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("secrets"); 
-        }
-    });
+
+    /*use this function to genrate only hashes of password without salting*/
+    // const newUser = new User({
+    //     email: req.body.username,
+    //     password: md5(req.body.password)
+    // });
+    // newUser.save(function (err) {
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     else{
+    //         res.render("secrets"); 
+    //     }
+    // });
 })
 
 app.post("/login", function (req, res) {
     const userEmail = req.body.username;
-    const userPassword = md5(req.body.password);
+    const userPassword = req.body.password;
+    //for creating hash using md5 package
+    // const userPassword = md5(req.body.password);
     User.findOne({email: userEmail}, function (err, foundUser) {
         if (foundUser === null || foundUser === ""){
             res.redirect("/register")
         }
         else{
-            if (userPassword === foundUser.password){
-                res.render("secrets");
-            }
+            bcrypt.compare(userPassword, foundUser.password, function(err, result) {
+                if(result === true){
+                    res.render("secrets");
+                }
+            });
+
+            /* used for hash password without salt */
+            // if (userPassword === foundUser.password){
+            //     res.render("secrets");
+            // }
             // else{
             //     res.render("/login");
             // }
