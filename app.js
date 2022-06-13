b0c31f335f09d1b622bd44680a3f308eefa5ea2f
Prankur Gupta <prankurgupta@Prankurs-MacBook-Pro.local>
2022-06-13 21:46:51 +0530

    Level 5 security with cookies and session using passport

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose"); 
 
-//encryption using bcrypt Hashing+Salting
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 //For using md5 hashing algorithms
 // const md5 = require("md5");
 
@@ -17,6 +17,15 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema =  new mongoose.Schema({
@@ -24,11 +33,18 @@ const userSchema =  new mongoose.Schema({
     password: String
 }); 
 
+userSchema.plugin(passportLocalMongoose);
+
 //encrypting the database passwords of user using mongoose-encryption
 /*userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});*/
 
 //creating collection using mongoose.model
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -42,22 +58,25 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
 app.post("/register", function(req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function (err) {
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.render("secrets"); 
-            }
-        });
+    User.register({username: req.body.username}, req.body.password, function (err, user) {
+        if(err) {
+            res.redirect("/register")
+        } else{
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
     });
+    
 
     /*use this function to genrate only hashes of password without salting*/
     // const newUser = new User({
@@ -75,30 +94,37 @@ app.post("/register", function(req, res) {
 })
 
 app.post("/login", function (req, res) {
-    const userEmail = req.body.username;
-    const userPassword = req.body.password;
-    //for creating hash using md5 package
-    // const userPassword = md5(req.body.password);
-    User.findOne({email: userEmail}, function (err, foundUser) {
-        if (foundUser === null || foundUser === ""){
-            res.redirect("/register")
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err) {
+        if (err) { 
+            console.log(err);
         }
         else{
-            bcrypt.compare(userPassword, foundUser.password, function(err, result) {
-                if(result === true){
-                    res.render("secrets");
-                }
-            });
-
-            /* used for hash password without salt */
-            // if (userPassword === foundUser.password){
-            //     res.render("secrets");
-            // }
-            // else{
-            //     res.render("/login");
-            // }
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });        
         }
-    })
+      });
+    //for creating hash using md5 package
+    // const userPassword = md5(req.body.password);
+    // User.findOne({email: userEmail}, function (err, foundUser) {
+    //     if (foundUser === null || foundUser === ""){
+    //         res.redirect("/register")
+    //     }
+    //     else{
+    //         /* used for hash password without salt */
+    //         // if (userPassword === foundUser.password){
+    //         //     res.render("secrets");
+    //         // }
+    //         // else{
+    //         //     res.render("/login");
+    //         // }
+    //     }
+    // })
 })
 
 
